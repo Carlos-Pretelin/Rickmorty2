1d1fa52f6fe43e00ae7bb974cfd048c1d4bda87d
Carlos Pretelin <carlosppretelin@gmail.com>
2022-11-10 08:59:15 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header.jsx";
+import Characters from "./components/Characters.jsx";
+
+function App() {
+  return (
+    <div className="App">
+     hola mundo  
+     <Header></Header>
+     <Characters></Characters>
+
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+
+const Header01 = () => {
+    const [darkmode, setDarkMode] = useState(false);
+
+    const handleClick = () => {
+        setDarkMode(!darkmode)
+
+    }
+    return(
+        <div className='Header'>
+            <h1>React Hooks</h1>
+            <button 
+            type='button'
+            onClick={handleClick}
+            >{darkmode ? "Dark Mode" : "Light Mode"}</button>
+        </div>
+    );
+}
+
+export default Header01;
