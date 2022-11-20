e05c6e33fffe79e2964fd03282510780f4a3ef29
Carlos Pretelin <carlosppretelin@gmail.com>
2022-11-20 06:27:18 -0600

    fix the characters component

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -5,17 +5,19 @@ const Characters = () => {
     const [characters, setCharacters] = useState([]);
 
     useEffect( () => {
-        fetch("https://rickandmortyapi.com/api/character/")
-        .then( response => response.json)
+        fetch('https://rickandmortyapi.com/api/character/')
+        .then( response => response.json())
         .then(data => setCharacters(data.results));
         
     }, []);
+
+
     console.log(characters)
     return(
         <div className='Characters'>
-            {characters.map((character) => {
+            {characters.map((character) => (
                 <h2>{character.name}</h2>
-            })}
+           ) )}
             
         </div>
     );
